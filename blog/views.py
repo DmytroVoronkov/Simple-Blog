2fda230adf9ce01e7d89d2d08831f0e456509dfb
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-04 11:13:53 +0300

    + basic urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+def starting_page(request):
+    return HttpResponse()
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request, slug):
+    pass
