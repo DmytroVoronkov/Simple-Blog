c030fa64f62549eff64f6bb4a72017d13b7165ca
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-04 19:19:14 +0300

    + insert data into page details template

--- blog/views.py
+++ blog/views.py
@@ -68,19 +68,22 @@ all_posts = [
     },
 ]
 
+
 def get_date(post):
-    return post['date']
+    return post["date"]
+
 
 # Create your views here.
 def starting_page(request):
     sorted_posts = sorted(all_posts, key=get_date)
     latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {'posts': latest_posts})
+    return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {"posts": all_posts})
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {"post": post})
