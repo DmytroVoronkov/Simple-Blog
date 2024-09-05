f5254b8ae625c79921a4cb71aa4e734b9c3f82d5
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-05 14:29:52 +0300

    + slugs

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,8 @@
+from typing import Iterable
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -10,6 +13,14 @@ class Book(models.Model):
     )
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
 
     def __str__(self) -> str:
         return f"{self.title} ({self.rating})"
+
+    def get_absolute_url(self):
+        return reverse("book-detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs) -> None:
+        self.slug = slugify(self.title)
+        return super().save(args, kwargs)
