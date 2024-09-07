e2b1e7802b2c44816bb6443740567a584ea15143
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-07 18:14:31 +0300

    + post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    slug = models.SlugField(unique=True, blank=True, editable=False)
+    content = models.TextField(validators=[MinLengthValidator(20)])
+
+    def __str__(self) -> str:
+        return self.slug
