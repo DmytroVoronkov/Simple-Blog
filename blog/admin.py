f69965a55128fa2b3da5c8b140c76d2fa67408ee
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-07 18:51:29 +0300

    + added models to admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import Author, Post, Tag
 
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date")
+    list_display = ("title", "author")
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -24,10 +24,12 @@ class Post(models.Model):
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True, auto_now_add=False)
-    slug = models.SlugField(unique=True, blank=True, editable=False)
+    slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(20)])
 
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
     tags = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
