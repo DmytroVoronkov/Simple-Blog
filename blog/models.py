87181f200bdc6067a61cff05056380efda5bd97d
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-12 09:13:10 +0300

    + file column in database

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True, auto_now_add=False)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(20)])
