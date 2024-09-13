df6e4cb9333e27b69a9c1538702ff55ee6c60a60
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-13 11:46:57 +0300

    + Creating Comments

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from datetime import date
 from typing import Any
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from django.views import View
 
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
 
 
@@ -36,9 +38,22 @@ class PostDetailView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
         }
         return render(request, "blog/post-detail.html", context)
 
-    def post(self, request):
-        pass
+    def post(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment: Comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+        }
+        return render(request, "blog/post-detail.html", context)
